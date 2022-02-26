d133e47f6df81c6a8fe898b4150bda7009d7b206
alina <kim.alina326@gmail.com>
2022-02-25 20:34:42 -0800

    Added abstract Board methods and hashable coordinates and also a defunct gitignore

--- src/tmge/objects/game/Board.java
+++ src/tmge/objects/game/Board.java
@@ -1,5 +1,27 @@
 package tmge.objects.game;
 
-public class Board {
+import java.util.Set;
 
+import tmge.objects.tokens.Coordinate;
+
+public abstract class Board {
+
+	public Set<Coordinate> selected;
+	
+	public abstract boolean update();
+	
+	public abstract boolean select(Coordinate location);
+	public abstract boolean deselect(Coordinate location);
+	public abstract void selectAll();
+	public abstract void deselectAll();
+	
+	public abstract boolean shift(Coordinate location, Coordinate vector);
+	public abstract boolean rotate(Coordinate location, boolean direction);
+	public abstract boolean remove(Coordinate location);
+	public abstract boolean put(Coordinate location, Tile tile);
+	public abstract boolean swap(Coordinate location1, Coordinate location2);
+	
+	public abstract boolean shiftSelected(Coordinate vector);
+	public abstract boolean rotateSelected(boolean direction);
+	
 }
--- src/tmge/objects/tokens/Coordinate.java
+++ src/tmge/objects/tokens/Coordinate.java
@@ -2,4 +2,30 @@ package tmge.objects.tokens;
 
 public class Coordinate {
 
+	public int x;
+	public int y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public boolean equals(Object c) {
+		if( c instanceof Coordinate ) {
+			return ((Coordinate) c).x == x && ((Coordinate) c).y == y;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		return toString().hashCode();
+	}
+	
+	public String toString() {
+		return String.join(String.join(",", x+"", y+""), "(", ")");
+	}
+	
 }
