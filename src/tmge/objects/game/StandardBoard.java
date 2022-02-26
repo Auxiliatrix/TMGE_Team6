9d8e4a8c42f03ed8d8895f05b29c194e8d727201
alina <kim.alina326@gmail.com>
2022-02-25 20:38:35 -0800

    trying to fix staged files

--- src/tmge/objects/game/StandardBoard.java
+++ src/tmge/objects/game/StandardBoard.java
@@ -2,4 +2,13 @@ package tmge.objects.game;
 
 public class StandardBoard {
 
+	public int height;
+	public int width;
+	
+	public Tile[][] tiles;
+	
+	public StandardBoard(int height, int width) {
+		this.tiles = new Tile[height][width];
+	}
+	
 }
