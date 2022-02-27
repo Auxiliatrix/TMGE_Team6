9db259440b8e296629aaedf1c55de96fd5111776
alina <kim.alina326@gmail.com>
2022-02-27 01:10:58 -0800

    Used classes in main class

--- src/tmge/main/Constants.java
+++ src/tmge/main/Constants.java
@@ -1,5 +1,9 @@
 package tmge.main;
 
 public class Constants {
-
+	
+	public static final int BOARD_HEIGHT = 20;
+	public static final int BOARD_WIDTH = 10;
+	public static final String DEFAULT_USER = "user";
+	
 }
--- src/tmge/main/Main.java
+++ src/tmge/main/Main.java
@@ -1,9 +1,20 @@
 package tmge.main;
 
+import tmge.game.base.GameEnvironment;
+import tmge.game.base.Player;
+import tmge.game.tiles.TiledBoard;
+import tmge.ui.UserInterface;
+
 public class Main {
 
 	public static void main(String[] args) {
+		UserInterface ui = null;
+		// TODO: Set UI to implementation
+		TiledBoard tb = new TiledBoard(Constants.BOARD_HEIGHT, Constants.BOARD_WIDTH);
+		Player player = Player.createNew(Constants.DEFAULT_USER);
+		GameEnvironment ge = new GameEnvironment(tb, player);
 		
+		// ui.display(tb);
 	}
 	
 }
