190f817e34eeae045e7e976e5b2f63c2877a69d2
Alina Kim <Squeakadoodle6084@gmail.com>
2022-03-04 15:04:43 -0800

    Implemented some game logic for Bejeweled engine

--- src/tmge/game/bejeweled/BejeweledEngine.java
+++ src/tmge/game/bejeweled/BejeweledEngine.java
@@ -1,5 +1,7 @@
 package tmge.game.bejeweled;
 
+import java.awt.Color;
+
 import tmge.game.base.GameEngine;
 import tmge.game.base.Player;
 import util.tokens.Coordinate;
@@ -7,7 +9,16 @@ import util.tokens.CoordinateGroup;
 
 public class BejeweledEngine extends GameEngine {
 
-	protected static final Coordinate GRAVITY_VECTOR = new Coordinate(1,0);
+	public static final Coordinate GRAVITY_VECTOR = new Coordinate(1,0);
+	public static final Color[] COLORSET = new Color[] {
+		Color.RED,
+		Color.ORANGE,
+		Color.YELLOW,
+		Color.GREEN,
+		Color.BLUE,
+		Color.MAGENTA,
+		Color.WHITE
+	};
 	
 	protected BejeweledBoard state;
 	protected Player player;
@@ -20,7 +31,11 @@ public class BejeweledEngine extends GameEngine {
 
 	@Override
 	public boolean tick() {
-		
+		if( gravity() ) {
+			spawn();
+		} else {
+			score();
+		}
 		
 		return false;
 	}
@@ -28,17 +43,41 @@ public class BejeweledEngine extends GameEngine {
 	protected boolean gravity() {
 		CoordinateGroup selected = new CoordinateGroup();
 		for( int f=state.getHeight()-1; f>=0; f-- ) {
-			selected.addAll(state.getAll(c -> {
-				Coordinate target = c.plus(GRAVITY_VECTOR);
-				return state.inBounds(target) && state.get(target) == state.getDefault();
-			}));
+			final int y = f;
+			selected.addAll(
+					state.getAll().getAll(c -> c.y == y).getAll(c -> {
+						Coordinate target = c.plus(GRAVITY_VECTOR);
+						return state.inBounds(target) && (state.get(target) == state.getDefault() || selected.contains(target));
+					})
+				);
 		}
 		
 		if( selected.size() > 0 ) {
 			state.shiftSelected(selected, GRAVITY_VECTOR);
-		} else {
-			
+			return true;
 		}
+		return false;
+	}
+	
+	protected boolean spawn() {
+		CoordinateGroup selected = state.getAll().getAll(c -> c.y == 0 && state.get(c) == state.getDefault());
+		if( selected.size() > 0 ) {
+			for( Coordinate coord : selected ) {
+				state.put(coord, getRandomValidColor());
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	protected boolean score() {
+		// TODO: implement scoring
+		return false;
+	}
+	
+	public static Color getRandomValidColor() {
+		int selection = (int) (Math.random() * COLORSET.length);
+		return COLORSET[selection];
 	}
 
 }
