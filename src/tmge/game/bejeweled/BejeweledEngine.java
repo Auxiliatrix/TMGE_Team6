0985ddce77ade719cdf3cb2920c7fa3a60b827c4
Alina Kim <Squeakadoodle6084@gmail.com>
2022-03-04 14:26:52 -0800

    Still incrementing

--- src/tmge/game/bejeweled/BejeweledEngine.java
+++ src/tmge/game/bejeweled/BejeweledEngine.java
@@ -1,18 +1,21 @@
 package tmge.game.bejeweled;
 
-import tmge.game.base.Board;
 import tmge.game.base.GameEngine;
 import tmge.game.base.Player;
 
 public class BejeweledEngine extends GameEngine {
 
-	public BejeweledEngine(Board initialState, Player player) {
-		super(initialState, player);
+	protected BejeweledBoard state;
+	protected Player player;
+	
+	public BejeweledEngine(BejeweledBoard initialState, Player player) {
+		super();
+		this.state = initialState;
+		this.player = player;
 	}
 
 	@Override
 	public boolean tick() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
