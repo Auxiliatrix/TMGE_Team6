d0f2dd07edd470f4c3e5770f8914aa8e738b0bc4
Alina Kim <Squeakadoodle6084@gmail.com>
2022-03-03 21:56:11 -0800

    Made Tile non-abstract and added basic type structure

--- src/tmge/game/tiles/Tile.java
+++ src/tmge/game/tiles/Tile.java
@@ -3,4 +3,26 @@ package tmge.game.tiles;
 /**
  * Generic Tile object to fill spaces on Boards.
  */
-public abstract class Tile {}
+public class Tile<E> {
+	protected E type;
+	
+	protected Tile(E type) {
+		this.type = type;
+	}
+	
+	public E getType() {
+		return type;
+	}
+	
+	public void setType(E type) {
+		this.type = type;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if( o instanceof Tile<?> ) {
+			return type.equals(((Tile<E>) o).type);
+		}
+		return false;
+	}
+}
