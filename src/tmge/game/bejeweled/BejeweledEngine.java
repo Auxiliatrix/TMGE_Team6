559fd383f66f0f9a1e37a0640887b8bb65da6056
Alina Kim <Squeakadoodle6084@gmail.com>
2022-03-04 14:51:41 -0800

    Incrementing

--- src/tmge/game/bejeweled/BejeweledEngine.java
+++ src/tmge/game/bejeweled/BejeweledEngine.java
@@ -1,11 +1,9 @@
 package tmge.game.bejeweled;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import tmge.game.base.GameEngine;
 import tmge.game.base.Player;
 import util.tokens.Coordinate;
+import util.tokens.CoordinateGroup;
 
 public class BejeweledEngine extends GameEngine {
 
@@ -28,7 +26,7 @@ public class BejeweledEngine extends GameEngine {
 	}
 	
 	protected boolean gravity() {
-		Set<Coordinate> selected = new HashSet<Coordinate>();
+		CoordinateGroup selected = new CoordinateGroup();
 		for( int f=state.getHeight()-1; f>=0; f-- ) {
 			selected.addAll(state.getAll(c -> {
 				Coordinate target = c.plus(GRAVITY_VECTOR);
