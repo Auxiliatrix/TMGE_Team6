983ab66145c28e3b9161a68d9b02c15509b3e9dc
alina <kim.alina326@gmail.com>
2022-03-12 02:25:22 -0800

    Implemented more abstract functions to GameEngine

--- src/tmge/game/base/GameEngine.java
+++ src/tmge/game/base/GameEngine.java
@@ -1,20 +1,48 @@
 package tmge.game.base;
 
+import tmge.ui.WindowCloseReactable;
+
 /**
  * Abstract class with functions relevant to game logic.
  */
-public abstract class GameEngine {
+public abstract class GameEngine implements WindowCloseReactable {
 
 	public double score;
+	protected boolean kill;
 		
 	public GameEngine() {
 		score = 0;
+		kill = false;
 	}
 	
 	/**
-	 * Ticks forward the game logic.
-	 * @return Whether the program is finished
+	 * Run logic to determine whether game should stop. If it should, run closing operations.
+	 * @return Whether the game should continue.
 	 */
-	public abstract boolean tick();
+	public final boolean tick() {
+		if( kill || !update() ) {
+			close();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Causes tick to return false no matter what.
+	 */
+	public void signalClosed() {
+		kill = true;
+	}
+	
+	/**
+	 * Return whether the game is incompleted.
+	 * @return False when game is complete
+	 */
+	protected abstract boolean update();
+	
+	/**
+	 * Run final operations before closing the game.
+	 */
+	protected abstract void close();
 	
 }
